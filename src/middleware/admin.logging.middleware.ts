a7290e68d5979408dbbc09cd34c0d4060cdd5764
thisisadwaithsheejin <iadwaithsheejin@gmail.com>
2024-04-04 11:57:08 +0530

    admin,user middleware changed

--- src/middleware/admin.logging.middleware.ts
+++ src/middleware/admin.logging.middleware.ts
@@ -7,12 +7,35 @@ export class AdminLoggingMiddleware implements NestMiddleware {
   constructor(private readonly adminLogService: AdminLogService) {}
   
   async use(req: Request, res: Response, next: NextFunction) {
-    const { method , originalUrl , params } = req;
-    const description = `${method} request for ${originalUrl}`;
+    const { method , originalUrl } = req;
+    let description ='';
+    const idMatch = originalUrl.match(/\/admin\/([^/]+)/);
+    const id = idMatch?idMatch[1]:undefined;
+    switch(method){
+      case 'GET':
+        if(originalUrl==='/admin'){
+          description = 'GET request for all Admins';
+        }
+        else{
+          description = `GET request for a particular Admin ${id}`;
+        }
+        break;
+        case 'POST':
+          description = 'POST request to create a new Admin';
+          break;
+        case 'PUT':
+          description = `PUT request to update Admin ${id}`;
+          break;
+        case 'DELETE':
+          description = `DELETE request to delete Admin ${id}`;
+          break;
+        default:
+          description = 'UNKNOWN REQUEST' 
+    }
     const AdminlogData = {
       method,
       path: originalUrl,
-      id:params.id,
+      id,
       description, 
     };
     await this.adminLogService.admincreateLog(AdminlogData);
--- src/middleware/user.logging.middleware.ts
+++ src/middleware/user.logging.middleware.ts
@@ -7,16 +7,38 @@ export class UserLoggingMiddleware implements NestMiddleware{
     constructor(private readonly userLogService:UserLogService){}
     async use(req:Request,res:Response,next:NextFunction){
         // Extracting method, originalUrl, and params from the request object
-        const {method, originalUrl, params}=req;
-        const description = `${method} request for ${originalUrl}`;
-        // Constructing log data object
+        const {method, originalUrl}=req;
+        let description='';
+        const idMatch = originalUrl.match(/\user\/([^/]+)/)
+        const id=idMatch?idMatch[1]:undefined;
+        switch(method){
+            case 'GET':
+                if(originalUrl==='/user'){
+                    description = 'GET request for all Users';
+                }
+                else{
+                    description = `GET request for a particular User ${id}`;
+                }
+                break;
+                case 'POST':
+                    description = `POST request to create a new User`;
+                    break;
+                case 'PUT':
+                    description = `PUT request to update User ${id}`;
+                    break;
+                case 'DELETE':
+                    description = `DELETE request to delete User ${id}`;
+                    break;
+                default:
+                    description = 'UNKNOWN REQUEST'
+        }
         const UserlogData = {
             // HTTP request method
             method,
             // Full path of the request
             path:originalUrl,
             // ID parameter from the request
-            id:params.id,
+            id,
             description,
         };
         await this.userLogService.UsercreateLog(UserlogData);
